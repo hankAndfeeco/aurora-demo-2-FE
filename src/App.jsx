f8c511325290f2c7592e5e1235be2423efd2e806
Feeco <lifeike@gmail.com>
2024-06-20 22:25:51 -0600

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import { useDropzone } from "react-dropzone"
 import viteLogo from "/vite.svg"
@@ -9,6 +9,15 @@ import { styled } from "@mui/material/styles"
 import CloudUploadIcon from "@mui/icons-material/CloudUpload"
 import Backdrop from "@mui/material/Backdrop"
 import CircularProgress from "@mui/material/CircularProgress"
+import axios from "axios"
+import Paper from "@mui/material/Paper"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TablePagination from "@mui/material/TablePagination"
+import TableRow from "@mui/material/TableRow"
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -22,9 +31,96 @@ const VisuallyHiddenInput = styled("input")({
   width: 1,
 })
 
+const columns = [
+  { id: "acct_", label: "Acct_", minWidth: 170 },
+  { id: "date", label: "Date", minWidth: 100 },
+  {
+    id: "delivery_address",
+    label: "Delivery Address",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "gross_amount",
+    label: "Gross Amount",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "gst",
+    label: "GST",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "haz_mat",
+    label: "Haz Mat",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "litres",
+    label: "Litres",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "pricelitre",
+    label: "Pricelitre",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "reference",
+    label: "Reference",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "sk_fuel_tax",
+    label: "Sk Fuel Tax",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "sk_pst",
+    label: "Sk PST",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "tank_",
+    label: "Tank",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "total_amount",
+    label: "Total Amount",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+]
+
+function createData(name, code, population, size) {
+  const density = population / size
+  return { name, code, population, size, density }
+}
+
 function App(props) {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
-  const [data, setData] = useState([])
+  const [data, setData] = useState({})
   const [pending, setPending] = useState(false)
 
   const files = acceptedFiles.map((file) => (
@@ -33,15 +129,30 @@ function App(props) {
     </li>
   ))
 
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
+
   const parse = async () => {
     await setPending(true)
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json)
-        setData(json)
-        setPending(false)
-      })
+    axios({
+      url: `https://demoocr.runasp.net/v1/documents/parse`,
+      method: "post",
+      data: { file: acceptedFiles[0] },
+      headers: { "Content-Type": "multipart/form-data" },
+    }).then((response) => {
+      console.log(response?.data)
+      setData(response?.data)
+      setPending(false)
+    })
   }
 
   return (
@@ -69,6 +180,50 @@ function App(props) {
           <Button onClick={parse} variant="contained" color="success">
             start parsing
           </Button>
+        </>
+      )}
+      {data?.document_id && (
+        <>
+          <Paper sx={{ width: "100%", overflow: "hidden" }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data?.line_items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {columns.map((column) => {
+                          const value = row[column.id]
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number" ? column.format(value) : value}
+                            </TableCell>
+                          )
+                        })}
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={data?.line_items?.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
         </>
       )}
     </>
