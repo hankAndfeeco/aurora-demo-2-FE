b84a5ff752378e9ecee0f94fe157d94033b7afa3
Feeco <lifeike@gmail.com>
2024-06-20 19:36:22 -0600

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import "./App.css"
 import { Typography, Button } from "@mui/material"
 import { styled } from "@mui/material/styles"
 import CloudUploadIcon from "@mui/icons-material/CloudUpload"
+import Backdrop from "@mui/material/Backdrop"
+import CircularProgress from "@mui/material/CircularProgress"
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -22,7 +24,8 @@ const VisuallyHiddenInput = styled("input")({
 
 function App(props) {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
+  const [pending, setPending] = useState(false)
 
   const files = acceptedFiles.map((file) => (
     <li key={file.path}>
@@ -30,14 +33,22 @@ function App(props) {
     </li>
   ))
 
-  const parse = () => {
+  const parse = async () => {
+    await setPending(true)
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
-      .then((json) => console.log(json))
+      .then((json) => {
+        console.log(json)
+        setData(json)
+        setPending(false)
+      })
   }
 
   return (
     <>
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={pending}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <Typography variant="h1">Aurora demo 2</Typography>
       <section className="container">
         <div {...getRootProps({ className: "dropzone" })}>
